6ac3f8665d3c17567f653e401c544a060a454f1e
Dobrohotova <irina.dobrohotova@gmail.com>
2017-02-13 20:25:13 +0100

    Array manipulation

---
+++ homework2.php
@@ -0,0 +1,146 @@
+<?php
+
+$posts = [
+	[
+		'title' => 'Lorem ipsum dolor sit amet',
+		'content' => 'Suspendisse vestibulum tortor quis nulla mattis, nec blandit nisi vulputate. Aliquam consectetur maximus erat at vulputate. Vivamus varius iaculis purus nec sollicitudin. Phasellus volutpat dapibus purus et sodales. Morbi id ipsum purus. Maecenas pellentesque vitae lacus nec eleifend. Ut et porta magna. Suspendisse rhoncus purus ut hendrerit pretium. Praesent ut ex aliquet, porttitor enim nec, scelerisque mi. Vestibulum rutrum bibendum ultrices. Morbi eu diam nec eros sodales efficitur. Sed lobortis metus at turpis finibus, ut lacinia elit vestibulum. Curabitur finibus leo vitae purus accumsan tincidunt. Maecenas euismod dapibus elit, eu malesuada lorem rhoncus eget. Aliquam tempus nulla nulla, nec blandit augue scelerisque in. Duis iaculis dignissim rutrum.',
+		'tags' => ['php', 'mysql', 'apache', 'linux', 'html5', 'javascript', 'css3'],
+		'comments' => [
+			[
+				'author' => 'Pero Perovski',
+				'comment' => 'Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+			],
+			[
+				'author' => 'Janko Jankovski',
+				'comment' => 'Duis aute irure dolor in reprehenderit... :)'
+			],
+			[
+				'author' => 'Stanko Stankovski',
+				'comment' => 'Ut enim ad minim veniam'
+			],
+			[
+				'author' => 'Ivan Ivanovski',
+				'comment' => 'Quis nostrud exercitation ullamco laboris nisi ut aliquip ex?'
+			],
+			[
+				'author' => 'Petko Petkovski',
+				'comment' => 'Dolore eu fugiat nulla pariatur...'
+			],
+		]
+	],
+	[
+		'title' => 'Curabitur nec est tellus',
+		'content' => 'Pellentesque vel massa sit amet orci efficitur pharetra. Aliquam neque neque, fermentum sed nibh et, gravida ultrices dolor. Sed faucibus vehicula ultrices. Quisque blandit sodales consectetur. Curabitur eu rutrum augue, ac aliquet enim. Cras purus urna, ornare eget augue non, luctus cursus nulla. Aliquam eget nisl rhoncus, ultricies justo sed, malesuada magna. Sed a arcu ornare, mollis purus eget, eleifend magna. Integer turpis diam, ornare id rutrum in, iaculis sollicitudin sapien. Vestibulum non nibh lacinia, tincidunt neque id, pellentesque tellus. Sed interdum erat ac sem elementum tincidunt. Etiam mattis justo at scelerisque volutpat. Cras ac nisl faucibus, mollis magna eu, auctor odio. In eu pulvinar lorem. Curabitur nec est tellus.',
+		'tags' => ['mysql',  'html5', 'javascript', 'css3'],
+		'comments' => [
+			
+			[
+				'author' => 'Janko Jankovski',
+				'comment' => 'Duis aute irure dolor in reprehenderit... :)'
+			],
+			[
+				'author' => 'Stanko Stankovski',
+				'comment' => 'Ut enim ad minim veniam'
+			],
+			[
+				'author' => 'Petko Petkovski',
+				'comment' => 'Dolore eu fugiat nulla pariatur...'
+			],
+		]
+	],
+	[
+		'title' => 'Quisque fringilla libero nec pellentesque suscipit',
+		'content' => 'Aliquam dignissim ut sem accumsan ornare. Vestibulum libero ligula, dictum ut leo et, vestibulum venenatis purus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec ut neque in risus egestas tempus. Praesent ultricies eget enim in blandit. Pellentesque aliquam, erat quis congue convallis, purus felis rutrum magna, vitae consequat augue orci in enim. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi porttitor fringilla ligula, vitae dapibus arcu convallis in. Donec blandit turpis sit amet ullamcorper molestie. Quisque fringilla libero nec pellentesque suscipit. Vestibulum viverra, lacus ut lobortis rutrum, ante lorem interdum nulla, in varius tortor nisl consequat risus. Nam quam nunc, congue sit amet dignissim et, vehicula id ligula. Sed suscipit eros magna, eget dignissim risus dapibus ullamcorper. Mauris tincidunt tortor id nibh condimentum faucibus. Curabitur varius imperdiet eleifend.',
+		'tags' => ['php', 'mysql', 'apache', 'linux'],
+		'comments' => [
+			[
+				'author' => 'Pero Perovski',
+				'comment' => 'Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+			],			
+			[
+				'author' => 'Petko Petkovski',
+				'comment' => 'Dolore eu fugiat nulla pariatur...'
+			],
+		]
+	],
+	[
+		'title' => 'Ut imperdiet sagittis faucibus',
+		'content' => 'Curabitur lobortis, odio ut bibendum suscipit, eros orci aliquam est, non commodo nulla leo fringilla odio. Integer ultricies iaculis ligula in suscipit. Phasellus convallis mi quis nibh imperdiet, ac tempus ligula euismod. Nullam vitae blandit risus. Phasellus nibh risus, semper id elementum eu, viverra in ex. Quisque id elit eu orci pulvinar egestas non nec enim. Ut imperdiet sagittis faucibus. Pellentesque condimentum mi a sapien molestie, sit amet mattis neque cursus. Vivamus scelerisque erat nec odio interdum vulputate. Ut sit amet malesuada turpis, id mollis tellus. Suspendisse lacinia laoreet malesuada. Etiam rutrum rutrum lorem, sit amet accumsan lacus finibus nec. Vivamus a neque luctus, mollis dui sed, gravida sem. Nulla neque purus, sollicitudin quis varius ut, facilisis non ipsum.',
+		'tags' => ['html5', 'javascript', 'css3'],
+		'comments' => [
+			[
+				'author' => 'Petko Petkovski',
+				'comment' => 'Dolore eu fugiat nulla pariatur...'
+			],
+		]
+	],
+	[
+		'title' => 'Nullam nec elit sit amet metus faucibus auctor',
+		'content' => 'Sed libero arcu, malesuada sed tellus sed, feugiat hendrerit magna. Pellentesque ultricies nisi nec sapien malesuada, sit amet mattis metus tempus. Sed suscipit elementum ipsum id tristique. Vivamus sed hendrerit est. Nulla porta accumsan felis sit amet rhoncus. Nullam nec elit sit amet metus faucibus auctor. Ut semper elit diam, sed scelerisque justo interdum non. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce et orci id leo euismod volutpat. Etiam in est et lectus maximus tempor. Nunc et quam diam. Duis tellus risus, interdum ut turpis et, elementum commodo ligula. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi aliquam massa placerat, cursus eros sed, maximus orci. Maecenas bibendum mi purus, sit amet hendrerit ligula ullamcorper eu.',
+		'tags' => ['javascript'],
+		'comments' => [
+			[
+				'author' => 'Pero Perovski',
+				'comment' => 'Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+			],
+			[
+				'author' => 'Janko Jankovski',
+				'comment' => 'Duis aute irure dolor in reprehenderit... :)'
+			],
+			[
+				'author' => 'Stanko Stankovski',
+				'comment' => 'Ut enim ad minim veniam'
+			],
+			[
+				'author' => 'Petko Petkovski',
+				'comment' => 'Dolore eu fugiat nulla pariatur...'
+			],
+		]
+	],
+	[
+		'title' => 'Praesent eget sem pretium',
+		'content' => ' Ut rutrum risus non enim interdum bibendum. Cras sollicitudin, magna et dignissim placerat, nunc erat egestas nisl, vel molestie orci magna ut tortor. Sed auctor venenatis lectus, a feugiat augue vestibulum ut. Nullam porttitor nunc magna, ut laoreet lorem fermentum ac. Praesent eget sem pretium, elementum justo nec, tristique urna. Phasellus blandit iaculis ligula. Suspendisse viverra tortor at ornare consequat. Aliquam ornare maximus efficitur. Praesent sit amet mauris ornare, placerat diam sit amet, aliquam quam. Aliquam erat volutpat. Fusce ac dolor gravida diam pulvinar porta eu et enim. Integer convallis ultricies lorem, ut egestas purus venenatis quis. Phasellus sit amet facilisis sapien. Proin ut massa sed tortor facilisis semper.',
+		'tags' => ['php', 'mysql', 'apache', 'linux', 'html5', 'javascript', 'css3'],
+		'comments' => [
+			[
+				'author' => 'Pero Perovski',
+				'comment' => 'Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+			],
+			[
+				'author' => 'Ivan Ivanovski',
+				'comment' => 'Quis nostrud exercitation ullamco laboris nisi ut aliquip ex?'
+			],
+			[
+				'author' => 'Petko Petkovski',
+				'comment' => 'Dolore eu fugiat nulla pariatur...'
+			],
+		]
+	],
+];
+
+?>
+
+
+<?php foreach($posts as $value){ ?>
+	<h1><?=$value['title']?></h1>
+	<p><?=$value['content']?></p>
+	<ul>
+		<?php foreach($value['tags'] as $tagovi){ ?>
+			<li><?=$tagovi?></li>
+		<?php } ?>
+	</ul>
+	<div>
+		<?php foreach($value['comments'] as $komentari){ ?>
+			<p><?=$komentari['author']?></p>
+			<p><?=$komentari['comment']?></p>
+		<?php } ?>
+	</div>
+<?php } ?>
+	
+
+
+
