a66c35775fc415b5a331dc4035f0c47839e3aa41
Dobrohotova <irina.dobrohotova@gmail.com>
2017-03-23 10:02:22 +0100

    with css

--- homework6.php
+++ homework6.php
@@ -1,14 +1,11 @@
+<style>
+	li {
+		float: left;
+		list-style: none;
+	}
+</style>
+
 <?php 
-
-
-
-// print_r($_FILES);
-
-
-for ($i=0; $i < count($_FILES['m']['type']); $i++) { 
-	$prefix = rand(1000000000, 9999999999);
-	move_uploaded_file($_FILES['m']['tmp_name'][$i], 'uploads/'.$prefix.'_'.$_FILES['m']['name'][$i]);
-}
 
 
 for($i = 0; $i < count($_FILES['m']['type']); $i++){
@@ -32,7 +29,7 @@ $dir = 'uploads/';
 $images = scandir($dir);
 
 
-print_r($images);
+// print_r($images);
 
 echo '<ul>';
 foreach ($images as $value) {
