3063c368be7adc6c87d39bbb4fe7d62d5e1ea8f4
Dobrohotova <irina.dobrohotova@gmail.com>
2017-05-17 19:17:19 +0200

    Blog posts in admin panel

---
+++ homework9blogposts.php
@@ -0,0 +1,35 @@
+<?php 
+session_start();
+
+$config = 'mysql:host=127.0.0.1;dbname=homework_db';
+$username = 'root';
+$password = '';
+$db = new PDO($config, $username, $password);
+
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Blog Posts</title>
+</head>
+<body>
+
+	<form action='homework9blogposts.php' method="post">
+		Title:<input type="text" name="title"><br><br>
+		Content:<textarea name="content"></textarea><br><br>
+		Publish Date:<input type="date" name="publish_date"><br><br>
+		Image:<input type="file" name="image"><br><br>
+		Category:<input type="text" name="id_category"><br><br>
+		<button type="submit">Submit</button>
+	</form>
+
+</body>
+</html>
